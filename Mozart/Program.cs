593b6468a13d7ff67d846a1c39d407b05ef71810
Rasmus <Rasmus@vinperlen.dk>
2021-01-26 14:59:24 +0100

    Add project files.

---
+++ Mozart/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mozart
+{
+    class Program
+    {
+        static SoundPlayer sp = new SoundPlayer();
+        static void Main(string[] args)
+        {
+            Minuetten();
+
+            Trioen();
+        }
+        static void Minuetten()
+        {
+            string path = @"C:\Users\rasmu\source\repos\Mozart\Mozart\Mozart\Wave\M";
+            string[] files = new string[177];
+
+            for (int i = 1; i < files.Length; i++)
+            {
+                files[i] = path + i + ".wav";
+                Console.WriteLine(files[i]);
+
+                sp.SoundLocation = files[i];
+                sp.Load();
+                sp.PlaySync();
+            }
+        }
+        static void Trioen()
+        {
+            string path = @"C:\Users\rasmu\source\repos\Mozart\Mozart\Mozart\Wave\T";
+            string[] files = new string[97];
+
+            for (int i = 1; i < files.Length; i++)
+            {
+                files[i] = path + i + ".wav";
+                Console.WriteLine(files[i]);
+
+                sp.SoundLocation = files[i];
+                sp.Load();
+                sp.PlaySync();
+            }
+        }
+    }
+}
